7753bc18f9497d3c7672bc4a24700b53b9f40480
Arthur <arthur.fr.sc@gmail.com>
2024-09-11 09:13:15 -0300

    classe nova

---
+++ Primeiro/src/Conexao.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+public static Connection getConexaoMySQL() {
+	Connection connection = null;
+	// driver de conexão
+	String driverName = "com.mysql.cj.jdbc.Drive";
+	try {
+	Class.forName(driverName);
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+		System.out.println("O driver especificado não foi encontrado");
+	}
+	String serverName = "localhost";
+	String mydatabase = "teste";
+	String url = "jdbc://mysql://" + serverName + "/" + mydatabase;
+	
+	String username = "root";
+	
+	String password = "aluno";
+	
+	try {
+		connection = DriverManager.getConnection(url, username, password);
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		System.out.println("não foi possivel se conectar");
+	}
+	
+	
+	
+	return connection;
+}
+}
