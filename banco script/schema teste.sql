b7a0932420a1ba6d37f5bdd2ecf3f3fb8ecf4995
Arthur <arthur.fr.sc@gmail.com>
2024-09-18 08:39:41 -0300

    finalmente banco exportado certo

--- banco script/schema teste.sql
+++ banco script/schema teste.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `teste` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `teste`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: teste
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-11 10:46:24
+-- Dump completed on 2024-09-18  8:38:19
